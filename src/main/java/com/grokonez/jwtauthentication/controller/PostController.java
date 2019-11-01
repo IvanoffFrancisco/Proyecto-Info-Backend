bd1618cadfcf88abed733e350874b93024ae9669
IvanoffFrancisco <nicoo.ivanoff@gmail.com>
2019-11-01 02:44:11 -0300

    se arreglo upload de imagenes

--- src/main/java/com/grokonez/jwtauthentication/controller/PostController.java
+++ src/main/java/com/grokonez/jwtauthentication/controller/PostController.java
@@ -1,29 +1,23 @@
 package com.grokonez.jwtauthentication.controller;
-
-import com.grokonez.jwtauthentication.repository.PostRepository;
 import com.grokonez.jwtauthentication.security.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/fotos")
 public class PostController {
 
     @Autowired
     private PostService postService;
 
-
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void uploadFile(@RequestParam MultipartFile foto){
-
-        postService.uploadFile(foto);
+    @PostMapping
+    public String upload(@RequestParam("foto") MultipartFile foto) throws Exception {
+      String asdasd= String.valueOf(postService.uploadData(foto));
+      return  asdasd;
     }
 
 
--- src/main/java/com/grokonez/jwtauthentication/security/services/PostService.java
+++ src/main/java/com/grokonez/jwtauthentication/security/services/PostService.java
@@ -1,13 +1,20 @@
 package com.grokonez.jwtauthentication.security.services;
 
 import com.grokonez.jwtauthentication.repository.PostRepository;
-import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+
+import org.apache.tomcat.jni.File;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.logging.Logger;
 
 @Service
 public class PostService {
@@ -15,11 +22,18 @@ public class PostService {
     @Autowired
     private PostRepository postRepository;
 
-    //private static final Logger logger = LoggerFactory.getLogger(PostService.class);
 
-    public ResponseEntity uploadFile(MultipartFile file) {
-        //logger.info(String.format("File name '%s' uploaded successfully.", file.getOriginalFilename()));
-        String asd=file.getOriginalFilename();
-        return ResponseEntity.ok().build();
+
+    public Path uploadData( MultipartFile file) throws Exception {
+        Path rutaCompleta=null;
+        if(!file.isEmpty()){
+            Path directorioRecursos= Paths.get("src//main//resources//static/images");
+            String rootPath=directorioRecursos.toFile().getAbsolutePath();
+            byte[] bytes=file.getBytes();
+            rutaCompleta=Paths.get(rootPath+"//"+file.getOriginalFilename());
+            Files.write(rutaCompleta,bytes);
+
+        }
+        return rutaCompleta;
     }
 }
